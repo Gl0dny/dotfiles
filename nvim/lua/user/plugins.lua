ed51bd866be0df2f9c02f31594ecb1c46bda11fc
Gl0dny <krystian.glodek1717@gmail.com>
2022-11-04 00:18:32 +0100

    tokyonight cholorscheme

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -45,6 +45,10 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   
+  -- Colorschemes
+  use "lunarvim/colorschemes"
+  -- use "lunarvim/darkplus.nvim"
+  use 'folke/tokyonight.nvim'
 
 
   -- Automatically set up your configuration after cloning packer.nvim
